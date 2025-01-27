48723d13fe7150913da5e87cc091abd23729a7a3
Austin Seipp <aseipp@pobox.com>
2025-01-26 19:07:55 -0600

    buck: upgrade buck2 bins, undo devcontainer hack
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# The rust devcontainer feature sets these variables to /usr/local/bin, but then
-# buck2 unsets them when running builds, so it looks like the toolchains aren't
-# initialized and the build fails. instead use the default values, which are
-# $HOME
-export CARGO_HOME="$HOME/.cargo"
-export RUSTUP_HOME="$HOME/.rustup"
-echo 'unset CARGO_HOME' >> $HOME/.bashrc
-echo 'unset RUSTUP_HOME' >> $HOME/.bashrc
-rustup default stable
-
 # init if .jj dir is not present
 #
 # if it is, we assume they probably have a configured working copy, so don't do
